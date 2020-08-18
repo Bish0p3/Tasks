0b6e3f56a1dd2a4f1790932acae9a972c6bb085f
Borys W <46605796+Bish0p3@users.noreply.github.com>
2020-08-18 10:32:25 +0200

    sum
    done

---
+++ task1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        double num1 = 0; 
+        int counter = 0;
+        Random random = new Random();
+
+        do
+        {
+            num1 = random.NextDouble();
+            //Console.WriteLine(num1);
+            counter++;
+        }
+        while (num1 <= 0.90 || num1 >= 0.91);
+
+        Console.WriteLine("Number of iterations: {0}", counter);
+    }
+}
---
+++ task2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        double num1 = 0;
+        double countTotal = 0;
+        Random random = new Random();
+
+        for (int i = 0; i < 10000; i++)
+        {
+            int counter = 0;
+            do
+            {
+                num1 = random.NextDouble();
+                //Console.WriteLine(num1);
+                counter++;
+            }
+            while (num1 <= 0.90 || num1 >= 0.91);
+            //Console.WriteLine("Number of iterations: {0}", count);
+            countTotal += counter;
+        }
+        //Console.WriteLine(countall);
+        Console.WriteLine("Average number of iterations: {0}", countTotal/10000);
+    }
+}
